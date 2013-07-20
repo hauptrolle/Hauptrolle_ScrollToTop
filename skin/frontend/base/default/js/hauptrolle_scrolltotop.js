c39820b32e0b5dd6357246d35bf89dec1a44c5ba
Achim Rolle <rolle@hucke-media.de>
2013-07-20 21:22:30 +0200

    add fade in/out effect

--- skin/frontend/base/default/js/hauptrolle_scrolltotop.js
+++ skin/frontend/base/default/js/hauptrolle_scrolltotop.js
@@ -1,5 +1,15 @@
 document.observe('dom:loaded', function() {
     $(document.body).setAttribute('id', 'scroll_top');
+    $('hauptrolle_scrolltotop').hide();
+
+    Event.observe(window, 'scroll', function() {
+        if(self.pageYOffset > 400) {
+            new Effect.Appear('hauptrolle_scrolltotop', { duration: .3 });
+        } else {
+            new Effect.Fade('hauptrolle_scrolltotop', { duration: .3 });
+        }
+    });
+
     $('hauptrolle_scrolltotop').observe('click', function(event) {
         event.preventDefault();
         new Effect.ScrollTo('scroll_top', { duration: .3 });
